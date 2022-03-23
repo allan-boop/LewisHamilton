ccb8c8b27660782544058d05e973e316d69df898
SULEYMAN KUTUK <kutuk.suleymann@gmail.com>
2022-03-23 14:28:41 +0100

    new navbar

--- index.js
+++
@@ -1,6 +0,0 @@
-const action = document.querySelector(".menu-deroulant");
-const lis = document.querySelector(".sous-menu");
-
-action.addEventListener("click", function () {
-  lis.classList.toggle("visible");
-});
